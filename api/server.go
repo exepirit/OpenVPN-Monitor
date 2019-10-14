e348568c7213564238c454e3aed721c16b8273f0
etherin@aamon <in37her@protonmail.com>
2019-10-15 00:55:38 +0700

    code cleanup

--- api/server.go
+++ api/server.go
@@ -14,25 +14,6 @@ type Server struct {
 	Address string
 }
 
-type ConnectedClient struct {
-	CommonName         string `json:"name"`
-	RealAddress        string `json:"real_address"`
-	VirtualAddress     string `json:"virtual_address"`
-	VirtualIPv6Address string `json:"virtual_ipv6_address"`
-	BytesRX            int    `json:"bytes_rx"`
-	BytesTX            int    `json:"bytes_tx"`
-	ConnectedSince     int    `json:"connected_since"`
-	Username           string `json:"username"`
-	ClientID           int    `json:"client_id"`
-	PeerID             int    `json:"peer_id"`
-}
-
-type ServerStatus struct {
-	Version string            `json:"version"`
-	Time    int               `json:"time"`
-	Clients []ConnectedClient `json:"clients"`
-}
-
 func (server *Server) RequestStatus() (ServerStatus, error) {
 	d := net.Dialer{Timeout: 5 * time.Second}
 	conn, err := d.Dial("tcp", server.Address)
---
+++ api/types.go
@@ -0,0 +1,20 @@
+package api
+
+type ConnectedClient struct {
+	CommonName         string `json:"name"`
+	RealAddress        string `json:"real_address"`
+	VirtualAddress     string `json:"virtual_address"`
+	VirtualIPv6Address string `json:"virtual_ipv6_address"`
+	BytesRX            int    `json:"bytes_rx"`
+	BytesTX            int    `json:"bytes_tx"`
+	ConnectedSince     int    `json:"connected_since"`
+	Username           string `json:"username"`
+	ClientID           int    `json:"client_id"`
+	PeerID             int    `json:"peer_id"`
+}
+
+type ServerStatus struct {
+	Version string            `json:"version"`
+	Time    int               `json:"time"`
+	Clients []ConnectedClient `json:"clients"`
+}

39a2857fdc185494755a68effac7dc061d4fef2b
exepirit <exepir1t@protonmail.com>
2020-11-25 12:04:17 +0700

    Use cli instead of configuration file

--- cmd/openvpn-monitor/main.go
+++ cmd/openvpn-monitor/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 
@@ -10,36 +10,44 @@ import (
 	"github.com/exepirit/OpenVPN-Monitor/internal/openvpn"
 )
 
-type Configuration struct {
-	OpenVPNAddress string `json:"openvpn_address"`
-	HandleAddress  string `json:"http_address"`
-}
-
-func HandleHTTP(address string, server *openvpn.Server) {
+func HandleHTTP(address string, server *openvpn.Server) error {
 	httpSrv := gin.Default()
 	httpSrv.GET("/api/status", api.StatusHandler(server))
 	httpSrv.StaticFile("/", "static/index.html")
-	if err := httpSrv.Run(address); err != nil {
-		log.Fatal(err)
-	}
+	return httpSrv.Run(address)
 }
 
-func main() {
-	config := Configuration{}
-	cfgFile, err := os.Open("config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := json.NewDecoder(cfgFile).Decode(&config); err != nil {
-		log.Fatal(err)
-	}
-
-	server := openvpn.Server{Address: config.OpenVPNAddress}
+func appFunc(ctx *cli.Context) error {
+	server := openvpn.Server{Address: ctx.String("server")}
 	if err := server.Connect(); err != nil {
 		log.Fatal(err)
 	}
 	defer server.Close()
 
-	HandleHTTP(config.HandleAddress, &server)
+	return HandleHTTP(ctx.String("bind"), &server)
+}
+
+func main() {
+	app := &cli.App{
+		Name: "openvpn-monitor",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "server",
+				Aliases:     []string{"s"},
+				EnvVars:     []string{"OPENVPN_SERVER"},
+				Required:    true,
+				DefaultText: "localhost:7505",
+			},
+			&cli.StringFlag{
+				Name:        "bind",
+				Aliases:     []string{"b"},
+				DefaultText: "0.0.0.0:8000",
+			},
+		},
+		Action: appFunc,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/exepirit/OpenVPN-Monitor
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/urfave/cli/v2 v2.3.0
+)
